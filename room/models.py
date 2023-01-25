b310142397ca7af596ace58805f1b0e5db305dd0
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-25 15:19:27 +0700

    fix: envupdate

--- room/models.py
+++ room/models.py
@@ -5,6 +5,7 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
 
+
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
