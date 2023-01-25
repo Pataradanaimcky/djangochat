a6f556470db893c859d72375f41e2112b6e98e1a
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-25 12:22:56 +0700

    fix: refresh file

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for djangochat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
@@ -16,7 +7,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 import room.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangochat.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -24,5 +15,5 @@ application = ProtocolTypeRouter({
         URLRouter(
             room.routing.websocket_urlpatterns
         )
-    )                        
+    )
 })
